6fdf6cd95a41af00958786c70b88ef880007926d
Luana Venâncio <luagabriela060@gmail.com>
2023-07-02 21:40:18 -0300

    First class: Position

---
+++ src/board/Position.java
@@ -0,0 +1,24 @@
+package board;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Integer getRow() {
+		return row;
+	}
+	public Integer getColumn() {
+		return column;
+	}
+
+	@Override
+	public String toString() {
+		return "Row: " + row + ". Column: " + column + ".";
+	}
+	
+	}
