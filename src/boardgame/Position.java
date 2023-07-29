ad5e5b44752b161875c572fd74f65d9778948107
Luana Venâncio <luagabriela060@gmail.com>
2023-07-29 00:31:00 -0300

    Possible moves for rook and bishop, printing possible moves and exception treatment

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -15,8 +15,17 @@ public class Position {
 	public Integer getRow() {
 		return row;
 	}
+	
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	
 	public Integer getColumn() {
 		return column;
+	}
+	
+	public void setColumn(Integer column) {
+		this.column = column;
 	}
 	
 	public void setValues(Integer row, Integer column) {
