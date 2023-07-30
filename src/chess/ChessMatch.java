50b75c1f9595ea31a6276f16ea54b081046e251d
Luana Venâncio <luagabriela060@gmail.com>
2023-07-29 22:38:12 -0300

    Add captured pieces and captured pieces count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -13,6 +16,10 @@ public class ChessMatch {
 	private Board board;
 	private Color currentPlayer;
 	private Integer turn;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
+	
 	private Boolean check;
 	private Boolean checkmate;
 	private ChessPiece enPassantVunerable;
@@ -82,6 +89,11 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	
@@ -110,6 +122,7 @@ public class ChessMatch {
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition (column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 
 	public void initialSetup() {
