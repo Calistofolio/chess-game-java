d89bbf8a8aa81eb467a5cc83a7dab5ccdb3cc41c
Luana Venâncio <luagabriela060@gmail.com>
2023-09-16 16:19:25 -0300

    Queen moves added and rook bugfix

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -24,8 +24,9 @@ public class Rook extends ChessPiece {
 		
 		//above
 		p.setValues(position.getRow() - 1, position.getColumn());
-		if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
 		}
 		
 		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
